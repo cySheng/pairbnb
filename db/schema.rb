1b6ece45fa53b04e9daa5e064e80a8d40cca6613
cySheng <cyusheng93@gmail.com>
2017-03-26 21:08:04 +0800

    Added specific show page for a listing and also migrated file for Verifications on listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326081916) do
+ActiveRecord::Schema.define(version: 20170326130203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,23 +36,24 @@ ActiveRecord::Schema.define(version: 20170326081916) do
   end
 
   create_table "listings", force: :cascade do |t|
-    t.string   "name",                null: false
-    t.text     "description",         null: false
-    t.string   "house_rules",         null: false
-    t.string   "number_of_beds",      null: false
-    t.string   "number_of_guests",    null: false
-    t.string   "number_of_bedrooms",  null: false
-    t.string   "number_of_bathrooms", null: false
-    t.string   "street",              null: false
-    t.string   "city",                null: false
-    t.string   "state",               null: false
-    t.string   "country",             null: false
-    t.string   "price",               null: false
-    t.string   "date_start",          null: false
-    t.string   "date_end",            null: false
+    t.string   "name",                                null: false
+    t.text     "description",                         null: false
+    t.string   "house_rules",                         null: false
+    t.string   "number_of_beds",                      null: false
+    t.string   "number_of_guests",                    null: false
+    t.string   "number_of_bedrooms",                  null: false
+    t.string   "number_of_bathrooms",                 null: false
+    t.string   "street",                              null: false
+    t.string   "city",                                null: false
+    t.string   "state",                               null: false
+    t.string   "country",                             null: false
+    t.string   "price",                               null: false
+    t.string   "date_start",                          null: false
+    t.string   "date_end",                            null: false
     t.integer  "user_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "verification",        default: false
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
